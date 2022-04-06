38f88af5989d2c0c730665a54ec2a0a2ab401922
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-06 19:25:51 -0300

    Corrigido nome classe ex2

---
+++ src/main/java/com/example/demo/Exercicio2.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ */
+@Path("/ex2{vel}")
+@Singleton
+public class Exercicio2 {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Double sayHello(@PathParam("vel") Double vel) {
+        System.out.println("Convertendo velocidade de nós para km/h");
+
+        return vel*1.852;
+    }
+}
