17a304b557f82a46c8179300a132e4f698db50ac
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-06 19:05:41 -0300

    Conversao nós para km/h - GET/POST

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,16 +3,22 @@ package com.example.demo;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  */
-@Path("/ex1")
+@Path("/ex1{vel}")
 @Singleton
 public class HelloController {
 
     @GET
-    public String sayHello() {
-        return "Hello World";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Double sayHello(@PathParam("vel") Double vel) {
+        System.out.println("Convertendo velocidade de nós para km/h");
+
+        return vel*1.852;
     }
 }
